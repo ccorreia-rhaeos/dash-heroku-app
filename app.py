c751f88104a20a2e3fa0c4bc975b1057618fb366
Chase Correia <ccorreia@rhaeos.com>
2024-05-24 10:54:54 -0500

    updates

--- app.py
+++ app.py
@@ -2,14 +2,9 @@
 import os
 from random import randint
 
-import plotly.plotly as py
-from plotly.graph_objs import *
 
 import flask
-import dash
-from dash.dependencies import Input, Output, State, Event
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, callback, html, dcc
 
 
 # Setup the app
@@ -17,9 +12,29 @@ import dash_html_components as html
 # the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+app = Dash(__name__, server=server)
 
+def create_dash_layout(app):
 
+    # Set browser tab title
+    app.title = "Chase's Analysis App" 
+    
+    # Header
+    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
+
+    # Body 
+    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """)])
+
+    # Footer
+    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built within Python using [Dash](https://plotly.com/dash/)""")])
+    
+    # Assemble dash layout 
+    app.layout = html.Div([header, body, footer])
+
+    return app
+
+# Construct the dash layout
+create_dash_layout(app)
 # Put your Dash code here
 
 
